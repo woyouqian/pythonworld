4a50c274ddbdedab96261af345ffcb78bbf52435
woyouqian <2036272868@qq.com>
2019-12-07 22:00:25 +0800

    modify readme

--- readme.py
+++ readme.py
@@ -1,7 +1,9 @@
 # simple explanation!
 
 print("说明：")
+print('------'*18)
 print("1.这是一个仓库，存放简单的Python脚本；")
-print("2.所有代码使用‘Python3’开发运行；")
-print("3.本人也只是菜鸟级，完全出于喜好；")
+print("2.所有代码使用‘Python3’开发并运行；")
+print("3.可执行性第一，简约规范性其次；")
+print("4.本人也只是菜鸟级，完全出于喜好。")
 print()
