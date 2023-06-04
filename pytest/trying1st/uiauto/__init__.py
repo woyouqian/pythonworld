f0645f628290fe49705d4ab7ee66096240a606af
woyouqian <2036272868@qq.com>
2023-06-04 22:25:53 +0800

    del from this lib

--- pytest/trying1st/uiauto/__init__.py
+++
@@ -1,6 +0,0 @@
-# 不包含APP自动化测试，自己不懂，也暂停了解学习！
-# postman		done
-# jmeter	性能测试，停止，菜鸟不可能达到……
-# loadrunner	性能测试，stop
-# Jenkins   为应付有变，先了解学习pytest框架与此工具！！！
-
--- pytest/trying1st/uiauto/producttest/test_product01.py
+++
@@ -1,9 +0,0 @@
-class Testproduct1(object):
-
-    def test_param(self, env_fixture):
-        print("it is testing param now 1...")
-        books = env_fixture['life']
-        for book in books:
-            print(book['name'], end='\t\t')
-            print(book['author'])
-
